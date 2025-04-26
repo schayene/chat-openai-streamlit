41ee2c6e0b2bfc2b6aff0a1890f3a1ddcf878f48
Schayene Royer <schayeness@gmail.com>
2025-04-26 10:49:18 -0300

    chore: structure and chat model LLM with openai and streamlit created

---
+++ README.md
@@ -0,0 +1,45 @@
+# Chat Streamlit + OpenAI
+
+![Project image](assets/chat.png)
+
+## Getting Started
+
+### Set up the virtual environment
+
+Create and activate a virtual environment:
+
+```bash
+# Create a virtual environment (using venv)
+python3 -m venv .venv
+
+# Activate the virtual environment
+# On Windows
+.venv\Scripts\activate
+# On macOS/Linux
+source .venv/bin/activate
+```
+
+Then, install the required dependencies:
+
+```bash
+pip install -r requirements.txt
+```
+
+### Create the 'secrets.toml' file
+
+Copy the sample secrets file and fill in your secret values:
+
+```bash
+cp secrets-sample.toml .streamlit/secrets.toml
+# Then edit secrets.toml to add your secrets
+```
+
+Alternatively, manually create the secrets.toml file inside the .streamlit folder based on the secrets-sample.toml file.
+
+### Run the development environment
+
+Start the Streamlit app with:
+
+```bash
+streamlit run streamlitGPT.py
+```
---
+++ streamlitGPT.py
@@ -0,0 +1,38 @@
+import streamlit as st
+from openai import OpenAI
+
+st.title("ChatGPT Like")
+
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+if "openai_model" not in st.session_state:
+  st.session_state["openai_model"] = "gpt-4.1-nano"
+
+if "messages" not in st.session_state:
+  st.session_state["messages"] = []
+
+for message in st.session_state.messages:
+  with st.chat_message(message["role"]):
+    st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+  instructions = "Responda as perguntas do usuário de maneira informal"
+
+  st.session_state.messages.append({"role": "user", "content": prompt})
+  
+  with st.chat_message("user"):
+    st.markdown(prompt)
+
+  with st.chat_message("assistant"):
+    stream = client.chat.completions.create(
+      model=st.session_state["openai_model"],
+      messages=[
+        {"role": "user", "content": prompt},
+        {"role": "system", "content": instructions}
+      ],
+      stream=True
+    )
+  
+    response = st.write_stream(stream)
+
+  st.session_state.messages.append({"role": "assistant", "content": response})
